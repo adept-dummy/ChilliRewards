ade7cd7bde0679d20a70af3b3fa52b22be4f9a16
mahendra-ap <mahendra.nc@adeptpros.com>
2015-06-19 10:12:32 +0530

    latest commit

--- ChiliRewards/src/com/example/chilirewards/SplashActivity.java
+++ ChiliRewards/src/com/example/chilirewards/SplashActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
 
 public class SplashActivity extends Activity {
 	
